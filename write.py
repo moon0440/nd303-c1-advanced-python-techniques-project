bf1d04185f176c1952887f176f10bb26717270ad
Blake Moon <blake.moon@gmail.com>
2021-09-22 02:14:47 -0500

    Removed TODO_DONE items from write.py

--- write.py
+++ write.py
@@ -24,8 +24,6 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO_DONE: Write the results to a CSV file, following the specification in the instructions.
-    # milestone: 4
     rows_dict = [r.serialize_to_csv for r in results]
     with open(filename, 'r') as f:
         writer = csv.DictWriter(f, fieldnames=rows_dict[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
@@ -44,8 +42,6 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO_DONE: Write the results to a JSON file, following the specification in the instructions.
-    # milestone: 4
     results_dicts = [r.serialize_to_json for r in results]
     with open(filename, 'w') as f:
         json.dump(results_dicts, f)
